6824bf9b587f06ddcb36579e4ea1c692382bc250
Ahmed <ahmedmohamed23345@gmail.com>
2020-04-01 14:15:41 +0200

    Initial commit

---
+++ app/src/main/java/com/endpoint/Aamr/models/LocationError.java
@@ -0,0 +1,14 @@
+package com.endpoint.Aamr.models;
+
+public class LocationError {
+
+    private int status;
+
+    public LocationError(int status) {
+        this.status = status;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
